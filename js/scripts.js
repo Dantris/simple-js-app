4f1297462b7dbf225df47b24284b8b83baceb174
Adrian <105604506+Dantris@users.noreply.github.com>
2023-08-02 10:02:35 +0200

    Added JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
-let pokemonList = [];
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison'],
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying'],
+  },
+  {
+    name: 'Squirtle',
+    height: 1,
+    types: ['water'],
+  },
+  {
+    name: 'Cubone',
+    height: 0.4,
+    types: ['Ground'],
+  },
+];
 
-// Each object represents a pokemon
-pokemonList.push({
-  name: 'Bulbasaur',
-  height: 7,
-  types: ['grass', 'poison'],
-});
+const specialHeightThreshold = 1;
 
-pokemonList.push({
-  name: 'Charmander',
-  height: 6,
-  types: ['fire'],
-});
+for (let i = 0; i < pokemonList.length; i++) {
+  const pokemon = pokemonList[i];
+  const name = pokemon.name;
+  const height = pokemon.height;
+  document.write(`${name} (height: ${height})`);
 
-pokemonList.push({
-  name: 'Squirtle',
-  height: 5,
-  types: ['water'],
-});
+  if (height > specialHeightThreshold) {
+    document.write(" - Wow, that's big!");
+  }
+
+  document.write('<br>'); // Add a line break after each Pokémon entry
+}
